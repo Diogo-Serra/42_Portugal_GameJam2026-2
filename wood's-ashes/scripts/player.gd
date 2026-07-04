b02974d806d90a2e657164bbc35aa16272d80d60
Diogo-Serra <di-code@outlook.com>
2026-07-04 02:40:29 +0100

    updates on map

--- wood's-ashes/scripts/player.gd
+++ wood's-ashes/scripts/player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-const SPEED = 120.0
+const SPEED = 6120.0
 
 func _physics_process(delta: float) -> void:
 	# Get input as a 2D vector (left/right and up/down combined)
 	var input_direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if input_direction != Vector2.ZERO:
-		velocity = input_direction * SPEED
+		velocity = input_direction * SPEED * delta
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	
