d818bcbff443e0f4c0732dfd189dd3d7ea357d5a
Henrique Carrasquinho <hcr2207@gmail.com>
2026-07-04 21:56:45 +0100

    Chest almost done and control menu added

---
+++ wood's-ashes/scripts/chest.gd
@@ -0,0 +1,44 @@
+extends StaticBody2D
+
+@onready var prompt_label: Label = $PromptLabel
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+const RewardPopup = preload("res://scenes/reward_popups.tscn")
+
+var player_in_range: bool = false
+var is_open: bool = false
+
+func _ready() -> void:
+	prompt_label.visible = false
+	$InteractionArea.body_entered.connect(_on_body_entered)
+	$InteractionArea.body_exited.connect(_on_body_exited)
+	sprite.frame = 0
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and not is_open:
+		player_in_range = true
+		prompt_label.visible = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = false
+		prompt_label.visible = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if player_in_range and not is_open and event.is_action_pressed("interact"):
+		open_chest()
+
+func open_chest() -> void:
+	is_open = true
+	prompt_label.visible = false
+	sprite.stop()
+	sprite.frame = 1 
+	var popup = RewardPopup.instantiate()
+	get_tree().root.add_child(popup)   # add to root so it's not affected by chest's position/pause
+	popup.life_chosen.connect(_on_life_chosen)
+	popup.damage_chosen.connect(_on_damage_chosen)
+	
+func _on_life_chosen() -> void:
+	print("Player chose Life!")
+
+func _on_damage_chosen() -> void:
+	print("Player chose Damage!")
